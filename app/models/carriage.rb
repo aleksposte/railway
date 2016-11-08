95170eeb727c266a84181b9f0d6d569517cbc6b3
aleksposte <aleks.poste@gmail.com>
2016-11-08 13:09:54 +0600

    Task 14, fix validates in the carriage

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,6 +1,11 @@
 class Carriage < ActiveRecord::Base
   belongs_to :train
 
+  validates :number, presence:  true
+  validates :top_seats, presence:  true
+  validates :bottom_seats, presence:  true
+  validates :train_id, presence:  true
+
   CARRIAGE_TYPES = ['Coupe', 'Econom']
 
   def self.type_carriage
